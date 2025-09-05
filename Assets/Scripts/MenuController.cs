ffa4d8993b2552c6f163f699da25d5a6ff5f95dd
Ssoar <jano20@vtr.net>
2025-09-05 10:17:45 -0400

    Hooking up Menu Buttons

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,12 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour{
+    [Header("Submenus")]
     [SerializeField] GameObject settingsCanvas;
     [SerializeField] GameObject menuCanvas;
+    [Header("Buttons")]
+    [SerializeField] Button startButton;
+    [SerializeField] Button quitButton;
     private bool settingsOpen = false;
+    void Start(){
+        SubscribeButtons();
+    }
     public void SwitchSettings(){
         settingsOpen = !settingsOpen;
         settingsCanvas.SetActive(settingsOpen);
         menuCanvas.SetActive(!settingsOpen);
+    }
+
+    void SubscribeButtons(){
+        if (startButton != null){
+            startButton.onClick.AddListener(SceneLoader.Instance.LoadGame);
+        }
+        if (quitButton != null){
+            quitButton.onClick.AddListener(SceneLoader.Instance.QuitGame);
+        }
     }
 }
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour{
+    //SINGLETON
+    public static SceneLoader Instance { get; private set;}
+    private void Awake(){
+        if (Instance != null && Instance != this){
+            Destroy(this);
+        }else{
+            Instance = this;
+        }
+    }
+    [SerializeField] string gameSceneName;
+    [SerializeField] string menuSceneName;
+    public void LoadGame(){
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void LoadMenu(){
+        SceneManager.LoadScene(menuSceneName);
+    }
+
+    public void QuitGame(){
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
+}
