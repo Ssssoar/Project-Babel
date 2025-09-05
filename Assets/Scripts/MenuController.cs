64f97655084a36be38948141819c892457dc834d
Ssoar <jano20@vtr.net>
2025-09-05 10:39:24 -0400

    AAAAAAAAA
    aaaaaaaaaaaaaa

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,17 +5,44 @@ public class MenuController : MonoBehaviour{
     [Header("Submenus")]
     [SerializeField] GameObject settingsCanvas;
     [SerializeField] GameObject menuCanvas;
+    [SerializeField] GameObject secretMenu;
     [Header("Buttons")]
     [SerializeField] Button startButton;
     [SerializeField] Button quitButton;
-    private bool settingsOpen = false;
+    enum Menu{Main,Setting,Secret};
+    private Menu currentMenu = Menu.Main;
     void Start(){
         SubscribeButtons();
     }
-    public void SwitchSettings(){
-        settingsOpen = !settingsOpen;
-        settingsCanvas.SetActive(settingsOpen);
-        menuCanvas.SetActive(!settingsOpen);
+
+    public void OpenSettingsMenu(){
+        if (currentMenu == Menu.Setting){
+            return;
+        }
+        currentMenu = Menu.Setting;
+        EnableMenuCanvases();
+    }
+
+    public void OpenMainMenu(){
+        if (currentMenu == Menu.Main){
+            return;
+        }
+        currentMenu = Menu.Main;
+        EnableMenuCanvases();
+    }
+
+    public void OpenSecretMenu(){
+        if (currentMenu == Menu.Secret){
+            return;
+        }
+        currentMenu = Menu.Secret;
+        EnableMenuCanvases();
+    }
+
+    void EnableMenuCanvases(){
+        settingsCanvas.SetActive(currentMenu == Menu.Setting);
+        menuCanvas.SetActive(currentMenu == Menu.Main);
+        secretMenu.SetActive(currentMenu == Menu.Secret);
     }
 
     void SubscribeButtons(){
