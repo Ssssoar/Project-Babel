9a315e604600ace2893da694bb93477c0ea68b29
Ssssoar <jano20@vtr.net>
2025-09-14 18:11:25 -0300

    finishing menu basics

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,14 +5,32 @@ public class MenuController : MonoBehaviour{
     [Header("Submenus")]
     [SerializeField] GameObject settingsCanvas;
     [SerializeField] GameObject menuCanvas;
-    [SerializeField] GameObject secretMenu;
+    [SerializeField] GameObject secretCanvas;
     [Header("Buttons")]
     [SerializeField] Button startButton;
+    [SerializeField] Button settingsButton;
     [SerializeField] Button quitButton;
+    [SerializeField] Button[] backToMenuButtons;
+    [SerializeField] Button[] secretMenuButtons;
+    [Header("Parameters")]
+    [SerializeField] float secretMenuTimeLimit;
+    //----ENUMS-----
     enum Menu{Main,Setting,Secret};
+    [Header("Variables")]
     private Menu currentMenu = Menu.Main;
+    private int correctSecretCombinationPresses = 0;
+    private float secretCombinationTimeLeft = 0f;
     void Start(){
         SubscribeButtons();
+    }
+
+    void Update(){
+        if (correctSecretCombinationPresses != 0){
+            secretCombinationTimeLeft -= Time.deltaTime;
+            if (secretCombinationTimeLeft <= 0f){
+                ResetSecretCombination();
+            }
+        }
     }
 
     public void OpenSettingsMenu(){
@@ -40,9 +58,27 @@ public class MenuController : MonoBehaviour{
     }
 
     void EnableMenuCanvases(){
-        settingsCanvas.SetActive(currentMenu == Menu.Setting);
-        menuCanvas.SetActive(currentMenu == Menu.Main);
-        secretMenu.SetActive(currentMenu == Menu.Secret);
+        if(settingsCanvas != null) settingsCanvas.SetActive(currentMenu == Menu.Setting);
+        if(menuCanvas != null) menuCanvas.SetActive(currentMenu == Menu.Main);
+        if(secretCanvas != null) secretCanvas.SetActive(currentMenu == Menu.Secret);
+    }
+
+    void SecretMenuButtonPress(Button buttonPressed){
+        if (buttonPressed == secretMenuButtons[correctSecretCombinationPresses]){
+            correctSecretCombinationPresses += 1;
+            secretCombinationTimeLeft = secretMenuTimeLimit;
+            if (correctSecretCombinationPresses == secretMenuButtons.Length){
+                ResetSecretCombination();
+                OpenSecretMenu();
+            }
+        }else{
+            ResetSecretCombination();
+        }
+    }
+
+    void ResetSecretCombination(){
+        correctSecretCombinationPresses = 0;
+        secretCombinationTimeLeft = 0f;
     }
 
     void SubscribeButtons(){
@@ -51,6 +87,19 @@ public class MenuController : MonoBehaviour{
         }
         if (quitButton != null){
             quitButton.onClick.AddListener(SceneLoader.Instance.QuitGame);
+        }
+        if (settingsButton != null){
+            settingsButton.onClick.AddListener(OpenSettingsMenu);
+        }
+        foreach(Button backToMenuButton in backToMenuButtons){
+            backToMenuButton.onClick.AddListener(OpenMainMenu);
+        }
+        foreach(Button secretMenuButton in secretMenuButtons){
+            secretMenuButton.onClick.AddListener(
+                delegate{
+                    SecretMenuButtonPress(secretMenuButton);
+                }
+            );
         }
     }
 }
